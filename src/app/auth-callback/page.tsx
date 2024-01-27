ebbd6ea4f980537e720e8657a77aeb3bb683b4ff
Indranil Halder <indranilhalder.dev@gmail.com>
2024-01-27 17:14:04 +0530

    Wrapped auth callback page in Suspense

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FC } from "react";
+import { FC, Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { trpc } from "../_trpc/client";
 import { Loader2 } from "lucide-react";
@@ -25,13 +25,15 @@ const AuthCallBackPage: FC = () => {
   }
 
   return (
-    <div className="mt-24 flex w-full justify-center">
-      <div className="flex flex-col items-center gap-2">
-        <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
-        <h3 className="text-xl font-semibold">Setting up your account...</h3>
-        <p>You will be redirected automatically.</p>
+    <Suspense>
+      <div className="mt-24 flex w-full justify-center">
+        <div className="flex flex-col items-center gap-2">
+          <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+          <h3 className="text-xl font-semibold">Setting up your account...</h3>
+          <p>You will be redirected automatically.</p>
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
